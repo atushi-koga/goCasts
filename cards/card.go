8712bec027e98ea5a4ddecdf3d1040372f52e26a
koga <koga@wh-plus.com>
2020-11-04 09:02:27 +0900

    cards deck

---
+++ cards/card.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+type deck []string
+
+func newDeck() deck {
+	var cards deck
+	cardsSuits := []string{"Spades", "Clubs", "Diamonds", "Hearts"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	for _, suit := range cardsSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) saveToFile(filename string) {
+	err := ioutil.WriteFile(filename, []byte(d.toString()), 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func deckFromReadFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() deck {
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < len(d); i++ {
+		randIndex := rand.Intn(len(d))
+
+		d[i], d[randIndex] = d[randIndex], d[i]
+	}
+
+	return d
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	card := cardContent()
-	fmt.Println(card)
-}
+	deck := newDeck()	// 同一パッケージ内では import 不要で別ファイルの関数を読み込める
+	hand, _ := deal(deck, 4)
+	fmt.Println(hand.shuffle())
 
-func cardContent() string {
-	return "Ace of Spades"
+	//fileName := "test_deck"
+	//hand.saveToFile(fileName)
+
+	//read := readFile(fileName)
+	//deck(strings.Split(string(read), ","))
+	//fmt.Println(deck)
+
+	//fmt.Println(deckFromReadFile("de"))
 }
