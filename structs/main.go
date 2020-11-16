f9dd3d4de6906cc8053ff88f1a46f86319379c42
koga <koga@wh-plus.com>
2020-11-16 09:57:48 +0900

    構造体personを作成

---
+++ structs/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName string
+}
+
+func main() {
+	var alex person
+
+	alex.firstName = "alex"
+	alex.lastName = "anderson"
+	fmt.Printf("%#v", alex)
+}
